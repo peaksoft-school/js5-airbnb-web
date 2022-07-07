4f0454395ee81262e5e872c2febd0b2f0d09c6d2
unknown <salievalmaz23@gmail.com>
2022-07-07 21:02:19 +0300

    repeat-development

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css'
 
 function App() {
-   return <div className="App" />
+   return (
+      <div className="App">
+         <p>development</p>
+      </div>
+   )
 }
 
 export default App
