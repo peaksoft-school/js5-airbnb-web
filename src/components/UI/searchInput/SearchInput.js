d3888eb2e42960ef7e0343f59f2fa6c5649f946f
Alina <alinaaliaskar51@gmail.com>
2022-07-08 12:11:00 +0600

    search input

--- src/components/UI/searchInput/SearchInput.js
+++ src/components/UI/searchInput/SearchInput.js
@@ -1,13 +1,41 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 import SearchIcon from '../../../assets/icons/searchIcon'
 // import searchIcon from '../assets/searchIcon2.svg'
 
+const data = [
+   { id: 1, region: 'Naryn' },
+   { id: 2, region: 'Osh' },
+   { id: 3, region: 'Talas' },
+   { id: 4, region: 'Chui' },
+   { id: 5, region: 'Batken' },
+   { id: 6, region: 'Jalal-Abad' },
+   { id: 7, region: 'Issyk-Kul' },
+]
+
 const SearchInput = (props) => {
+   const [filteredData, setFilteredData] = useState([])
+   const [wordEntered, setWordEntered] = useState('')
+
+   const filterHandler = (event) => {
+      const searchWord = event.target.value
+      setWordEntered(searchWord)
+      const newFilter = data.filter((value) => {
+         return value.region.toLowerCase().includes(searchWord.toLowerCase())
+      })
+
+      if (searchWord === '') {
+         setFilteredData([])
+      } else {
+         setFilteredData(newFilter)
+      }
+   }
    return (
       <StyledSearchInput>
          <Icon>
             <SearchIcon width="20px" height="20px" />
          </Icon>
+
          <input
             style={{
                width: props.width,
@@ -15,7 +43,16 @@ const SearchInput = (props) => {
                border: props.border,
             }}
             placeholder={props.placeholder}
+            onChange={filterHandler}
+            value={wordEntered}
          />
+         {filteredData.length !== 0 && (
+            <div>
+               {filteredData.slice(0, 15).map((value) => {
+                  return <p>{value.region} </p>
+               })}
+            </div>
+         )}
       </StyledSearchInput>
    )
 }
