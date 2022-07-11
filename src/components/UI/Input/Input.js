1c1eb1619e8e60400b70118d1f90f442b165e130
tarielgafurov <tarielgafurov618@gmail.com>
2022-07-11 18:01:23 +0300

    Input/Component/UI

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,18 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 
-const Input = React.forwardRef(( ref, props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
-    <>
-      <StyledInput
-        ref={ref}
-        width={props.width}
-        type={props.type}
-        placeholder={props.placeholder}
-        onChange={(e) => props.onChange(e.target.value)}
-        value={props.value}
-      />
-    </>
+    <StyledInput
+      forwardRef={ref}
+      {...props}
+    />
   );
 });
 
@@ -21,13 +15,13 @@ export default Input;
 const StyledInput = styled.input`
   width: ${(props) => props.width || "100%"};
   height: 39px;
+  padding: 10px 8px 10px 16px;
   &:hover {
     border: 1px solid #828282;
     border-radius: 2px;
   }
   &:active {
     border: 1px solid #828282;
-    /* border: 1px solid red; */
     border-radius: 2px;
   }
   border: 1px solid #c4c4c4;
