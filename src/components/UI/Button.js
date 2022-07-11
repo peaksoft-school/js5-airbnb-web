7d2112548c13be354b78f57683045f0f4195904a
mairamkul211 <mairamkulkeldibekuulu76@gmail.com>
2022-07-11 10:44:11 +0300

    props Button

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -19,22 +19,27 @@ const CustomButton = styled.button`
    width: ${(props) => props.width};
    height: ${(props) => props.height};
    font-size: ${(props) => props.fontSize};
-   background: ${(props) => (props.variant ? 'white' : '#dd8a08')};
+   background: ${(props) =>
+      props.variant === 'outlined' ? 'white' : '#dd8a08'};
    border-radius: 2px;
-   border: ${(props) => (props.variant ? '1px solid #828282;' : 'none')};
+   border: ${(props) =>
+      props.variant === 'outlined' ? '1px solid #828282;' : 'none'};
    font-weight: 500;
-   color: ${(props) => (props.variant ? '#828282' : '#f7f7f7')};
+   color: ${(props) => (props.variant === 'outlined' ? '#828282' : '#f7f7f7')};
    line-height: 17px;
    gap: 10px;
    cursor: pointer;
    &:hover {
-      background-color: ${(props) => (props.variant ? 'none' : '#a0522d')};
+      background-color: ${(props) =>
+         props.variant === 'outlined' ? 'none' : '#a0522d'};
       border: ${(props) =>
-         props.variant ? '1px solid rgb(100, 98, 98)' : 'none'};
+         props.variant === 'outlined' ? '1px solid rgb(100, 98, 98)' : 'none'};
    }
    &:active {
-      background-color: ${(props) => (props.variant ? '#f4a460' : '#dd8a08')};
+      background-color: ${(props) =>
+         props.variant === 'outlined' ? '#f4a460' : '#dd8a08'};
       color: #f7f7f7;
+      border: none;
    }
    &:disabled {
       background-color: #a9a9a9;
