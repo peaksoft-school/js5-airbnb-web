ee7fe38f67a581b407025af40d591a851c898a09
tarielgafurov <tarielgafurov618@gmail.com>
2022-07-11 15:50:04 +0300

    UI/Input/Component

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -5,11 +5,8 @@ const Input = (props) => {
   return (
     <div>
       <Inputt
-        style={{
-          width: props.width,
-          height: props.height,
-          backgroundColor: props.backgroundColor,
-        }}
+        width={props.width}
+        height={props.height}
         type={props.type}
         placeholder={props.placeholder}
         onChange={(e) => props.onChange(e.target.value)}
@@ -22,13 +19,22 @@ const Input = (props) => {
 export default Input;
 
 const Inputt = styled.input`
+  width: ${(props) => props.width} 414px;
+  height: ${(props) => props.height} 39px ;
+  &:hover {
+    border: 1px solid #828282;
+    border-radius: 2px;
+  }
+  &:active{
+    border: 1px solid #828282;
+  }
   border: 1px solid #c4c4c4;
   border-radius: 2px;
   outline: none;
   &::placeholder {
     width: 70px;
     height: 19px;
-    font-family: "Roboto";
+    font-family: "Inter";
     font-style: normal;
     font-weight: 400;
     font-size: 16px;
