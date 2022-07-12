fdccc4779d1d6439aaf04d5d4aa40a9f33cdd499
tarielgafurov <tarielgafurov618@gmail.com>
2022-07-12 12:10:25 +0300

    Input/Component/UI

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const Input = React.forwardRef((props, ref) => {
-  return <StyledInput forwardRef={ref} {...props} />;
+  return <StyledInput ref={ref} {...props}  />;
 });
 
 export default Input;
