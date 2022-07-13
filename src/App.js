1482f08d0256ed2dbec6fd53bade14f8467a9c90
mairamkul211 <mairamkulkeldibekuulu76@gmail.com>
2022-07-13 14:09:24 +0300

    add disabled variant contained

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css'
-import Button from './components/UI/Button'
 
 function App() {
    return (
       <div className="App">
          <p>development</p>
-         <Button width="150px" variant="contained">
-            add
-         </Button>
       </div>
    )
 }
