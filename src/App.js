787b260424301253949adeaf14870b8dea99a3c7
mairamkul211 <mairamkulkeldibekuulu76@gmail.com>
2022-07-13 23:05:05 +0300

    fixed rating star

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css'
-import Rating from './components/UI/Rating'
 
 function App() {
    return (
       <div className="App">
          <p>development</p>
-         <Rating defaultValue={4} />
       </div>
    )
 }
