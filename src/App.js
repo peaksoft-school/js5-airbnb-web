ccf0a43afba0a2100ee9536efe6af849d5f902df
mairamkul211 <mairamkulkeldibekuulu76@gmail.com>
2022-07-13 23:01:41 +0300

    fixed rating component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css'
+import Rating from './components/UI/Rating'
 
 function App() {
    return (
       <div className="App">
          <p>development</p>
+         <Rating defaultValue={4} />
       </div>
    )
 }
