cc48d9fa1862ef21f508d28dbe3b8ea9170aa7b9
Alina <alinaaliaskar51@gmail.com>
2022-07-13 12:26:30 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useState } from 'react'
 import './App.css'
+import cardPic from './assets/images/cardPic.png'
+import BlockedCard from './components/UI/cards/BlockedCard'
+import UserProfileAnnouncementCard from './components/UI/cards/UserProfileAnnouncementCard'
+// import SearchInput from './components/UI/searchInput/SearchInput'
 
 function App() {
+   const [blocked, setBlocked] = useState(false)
+
+   const openHandler = () => {
+      setBlocked(!blocked)
+   }
+
    return (
       <div className="App">
          <p>development</p>
+         <UserProfileAnnouncementCard open={blocked} url={cardPic} />
+         <button onClick={openHandler}>open</button>
+         {blocked && <BlockedCard open={blocked} onClose={setBlocked} />}
+         {/* <SearchInput
+            width="100px"
+            height="100px"
+            border="none"
+            placeholder="ffffff"
+         /> */}
       </div>
    )
 }
--- src/components/UI/searchInput/SearchInput.js
+++ src/components/UI/searchInput/SearchInput.js
@@ -1,58 +1,55 @@
-import { useState } from 'react'
-import styled from 'styled-components'
+// import { useState } from 'react'
+import styled, { css } from 'styled-components'
 import SearchIcon from '../../../assets/icons/searchIcon'
-// import searchIcon from '../assets/searchIcon2.svg'
 
-const data = [
-   { id: 1, region: 'Naryn' },
-   { id: 2, region: 'Osh' },
-   { id: 3, region: 'Talas' },
-   { id: 4, region: 'Chui' },
-   { id: 5, region: 'Batken' },
-   { id: 6, region: 'Jalal-Abad' },
-   { id: 7, region: 'Issyk-Kul' },
-]
+// const data = [
+//    { id: 1, region: 'Naryn' },
+//    { id: 2, region: 'Osh' },
+//    { id: 3, region: 'Talas' },
+//    { id: 4, region: 'Chui' },
+//    { id: 5, region: 'Batken' },
+//    { id: 6, region: 'Jalal-Abad' },
+//    { id: 7, region: 'Issyk-Kul' },
+// ]
 
 const SearchInput = (props) => {
-   const [filteredData, setFilteredData] = useState([])
-   const [wordEntered, setWordEntered] = useState('')
+   // const [filteredData, setFilteredData] = useState([])
+   // const [wordEntered, setWordEntered] = useState('')
 
-   const filterHandler = (event) => {
-      const searchWord = event.target.value
-      setWordEntered(searchWord)
-      const newFilter = data.filter((value) => {
-         return value.region.toLowerCase().includes(searchWord.toLowerCase())
-      })
+   // const filterHandler = (event) => {
+   //    const searchWord = event.target.value
+   //    setWordEntered(searchWord)
+   //    const newFilter = data.filter((value) => {
+   //       return value.region.toLowerCase().includes(searchWord.toLowerCase())
+   //    })
 
-      if (searchWord === '') {
-         setFilteredData([])
-      } else {
-         setFilteredData(newFilter)
-      }
-   }
+   //    if (searchWord === '') {
+   //       setFilteredData([])
+   //    } else {
+   //       setFilteredData(newFilter)
+   //    }
+   // }
    return (
       <StyledSearchInput>
          <Icon>
             <SearchIcon width="20px" height="20px" />
          </Icon>
 
-         <input
-            style={{
-               width: props.width,
-               height: props.height,
-               border: props.border,
-            }}
+         <Input
+            border={props.border}
+            width={props.width}
+            height={props.height}
             placeholder={props.placeholder}
-            onChange={filterHandler}
-            value={wordEntered}
+            // onChange={filterHandler}
+            // value={wordEntered}
          />
-         {filteredData.length !== 0 && (
+         {/* {filteredData.length !== 0 && (
             <div>
                {filteredData.slice(0, 15).map((value) => {
                   return <p>{value.region} </p>
                })}
             </div>
-         )}
+         )} */}
       </StyledSearchInput>
    )
 }
@@ -61,14 +58,31 @@ export default SearchInput
 
 const StyledSearchInput = styled.div`
    & input {
-      box-sizing: border-box;
-      background: #f7f7f7;
-      border-radius: 2px;
-      padding: 10px 40px;
    }
    & :focus {
       outline: none;
    }
+`
+const Input = styled.input`
+   box-sizing: border-box;
+   background: #f7f7f7;
+   border-radius: 2px;
+   padding: 10px 40px;
+   ${(props) =>
+      props.width &&
+      css`
+         width: ${props.width};
+      `}
+   ${(props) =>
+      props.height &&
+      css`
+         height: ${props.height};
+      `}
+   ${(props) =>
+      props.border &&
+      css`
+         border: ${props.border};
+      `}
 `
 
 const Icon = styled.div`
