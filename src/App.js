95eabeffbf34dd4caaf131173ef9f959ef577b64
Your Name <zukokalilov@gmail.com>
2022-07-16 00:01:48 +0600

    select

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css'
-import BasicSelect from './components/UI/BasicSelect'
 
 function App() {
    return (
       <div className="App">
          <p>development</p>
-         <BasicSelect />
       </div>
    )
 }
--- src/components/UI/Select.js
+++ src/components/UI/Select.js
@@ -1,45 +1,41 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Box from '@mui/material/Box'
 import FormControl from '@mui/material/FormControl'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
-import Select from '@mui/material/Select'
+import SelectMui from '@mui/material/Select'
 import styled from 'styled-components'
 
-export default function BasicSelect({ variant, onChange, arrayOptions }) {
-   const [region, setRegion] = useState('')
+function Select({
+   label,
+   valueOptions,
+   variant,
+   onChange,
+   options,
+   getOptionLabel,
+}) {
    const handleChange = (event) => {
-      setRegion(event.target.value)
       onChange(event.target.value)
    }
 
    return (
       <BoxStyled variant={variant}>
+         <InputLabel>{variant === 'sort' ? label : label}</InputLabel>
          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">
-               {variant === 'sort' ? 'Sort by' : 'Please, select the region'}
-            </InputLabel>
-            <Select
-               onChange={handleChange}
-               labelId="demo-simple-select-label"
-               id="demo-simple-select"
-               value={region}
-               label={
-                  variant === 'sort' ? 'Sort By' : 'Please, select the region'
-               }
-            >
-               {arrayOptions?.map((item) => {
+            <SelectMui onChange={handleChange} value={valueOptions}>
+               {options?.map((item) => {
                   return (
-                     <MenuItem key={item.id} value={item.region}>
-                        {item.region}
+                     <MenuItem value={item.getOptionValue}>
+                        {getOptionLabel(item)}
                      </MenuItem>
                   )
                })}
-            </Select>
+            </SelectMui>
          </FormControl>
       </BoxStyled>
    )
 }
+export default Select
 
 const BoxStyled = styled(Box)`
    width: ${(props) => (props.variant === 'sort' ? '271px' : '610px')};
