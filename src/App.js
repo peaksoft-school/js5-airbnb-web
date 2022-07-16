2087e07babe8c2f2625f93900acff8d6a49bade3
Your Name <zukokalilov@gmail.com>
2022-07-16 20:18:01 +0600

    comments select value

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css'
-import Select from './components/UI/Select'
 
 function App() {
    return (
       <div className="App">
          <p>development</p>
-         <Select />
       </div>
    )
 }
