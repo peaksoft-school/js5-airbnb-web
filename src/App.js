67c23021f98de170c94957ee6a263530baf3f134
zhanara2021 <buzurmankyzyzanara@gmail.com>
2022-07-29 19:13:03 +0600

    testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Select from './components/UI/Select'
+import UserHeader from './layout/headers/User-VendorHeader/UserHeader'
+import UserApartmentBody from './layout/UserApartmentBody/UserApartmentBody'
 import './App.css'
 
 function App() {
    return (
       <div className="App">
-         <p>development</p>
+         <UserHeader />
+         <Select />
+         <UserApartmentBody />
       </div>
    )
 }
---
+++ src/components/UI/Select.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import SelectMui from '@mui/material/Select'
+import styled from 'styled-components'
+
+function Select({
+   label,
+   value,
+   variant,
+   onChange,
+   options,
+   getOptionLabel,
+   getOptionValue,
+}) {
+   const handleChange = (event) => {
+      onChange(event.target.value)
+   }
+
+   return (
+      <BoxStyled variant={variant}>
+         <InputLabel>{label}</InputLabel>
+         <FormControl fullWidth>
+            <SelectMui onChange={handleChange} value={value}>
+               {options?.map((item) => {
+                  return (
+                     <MenuItem value={getOptionValue(item)}>
+                        {getOptionLabel(item)}
+                     </MenuItem>
+                  )
+               })}
+            </SelectMui>
+         </FormControl>
+      </BoxStyled>
+   )
+}
+export default Select
+
+const BoxStyled = styled(Box)`
+   width: ${(props) => (props.variant === 'sort' ? '271px' : '610px')};
+   height: 262px;
+   background: #ffffff;
+   border-radius: 2px;
+   & fieldset {
+      border: 1px solid #c4c4c4 !important;
+   }
+   @media (max-width: 375px) {
+      width: 343px;
+      height: 262px;
+      background: #ffffff;
+      border-radius: 2px;
+      margin: 20px;
+   }
+`
