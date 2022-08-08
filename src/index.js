cbe8fd5674988d4aaf625d6dff110b7a4bb6e6b0
Alina <alinaaliaskar51@gmail.com>
2022-08-08 17:23:45 +0600

    fixed

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import store from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-   <BrowserRouter>
-      <React.StrictMode>
+   <React.StrictMode>
+      <BrowserRouter>
          <Provider store={store}>
             <App />
          </Provider>
-      </React.StrictMode>
-   </BrowserRouter>
+      </BrowserRouter>
+   </React.StrictMode>
 )
