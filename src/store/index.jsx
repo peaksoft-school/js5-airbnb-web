59065c883b56b1502de40bbbf01a3da33c72e693
Your Name <zukokalilov@gmail.com>
2022-08-31 16:57:15 +0600

    edit Annountcemnet

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 // eslint-disable-next-line import/no-cycle
 import addAnnountcementSlice from './slices/addAnnountcementSlice'
+import editAnnountcementSlice from './slices/editAnnountcementSlice'
 import LoginSlice from './slices/LoginSlice'
 
 const Store = configureStore({
    reducer: {
       login: LoginSlice.reducer,
       addAnnountcement: addAnnountcementSlice.reducer,
+      editAnnountcement: editAnnountcementSlice.reducer,
    },
 })
 export default Store
