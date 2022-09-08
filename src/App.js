3b46569c83116e58be572f027c7b1fa290a5bbf5
User <user@MacBook-Pro-4.local>
2022-09-09 00:28:06 +0600

    user-profile

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import './App.css'
-import AddAnnouncementForm from './components/AddAnnountCementForm'
-import UserProfile from './components/user-profile/UserProfile'
-// eslint-disable-next-line import/order
-import { SignUp } from './components/SignUp'
-// eslint-disable-next-line import/order
 
 function App() {
    return (
       <div className="App">
-         <SignUp />
-         <AddAnnouncementForm />
-         <UserProfile />
-         {/* <p>development</p> */}
+         <p>development</p>
       </div>
    )
 }
