40c4f918c892c3fc4e9b43adcae349a909995ca1
User <user@MacBook-Pro-4.local>
2022-09-21 19:57:21 +0600

    test4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css'
+import AddAnnouncementForm from './components/AddAnnountCementForm'
+import SignUp from './components/SignUp'
 import UserProfile from './components/user-profile/UserProfile'
 
 function App() {
    return (
       <div className="App">
-         <p>development</p>
+         {/* <p>development</p> */}
+         <AddAnnouncementForm />
+         <SignUp />
          <UserProfile />
       </div>
    )
